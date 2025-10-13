cfd9a9859c71ba3b612e1b39ff72630b5ad95cc6
Austina247 <austinaanestad@gmail.com>
2025-10-13 18:39:03 -0400

    Lab6
    Signed-off-by: Austina247 <austinaanestad@gmail.com>

--- Lab6.py
+++ Lab6.py
@@ -1,11 +1,10 @@
 def initialize_board(num_rows, num_cols):
-    if __name__ == '__main__':
-        board = []
-        for i in range(num_rows):
-            board.append([])
-            for j in range(num_cols):
-                board[i].append("-")
-        return board
+    board = []
+    for i in range(num_rows):
+        board.append([])
+        for j in range(num_cols):
+            board[i].append("-")
+    return board
 
 def print_board(board):
     count = 0
@@ -42,32 +41,31 @@ def check_if_winner(board, col, row, chip_type):
         if count == 4:
             return True
     return False
-
-rows = int(input("What would you like the height of the board to be? "))
-cols = int(input("What would you like the length of the board to be? "))
-board = initialize_board(rows, cols)
-print_board(board)
-print("\n\nPlayer 1: x\nPlayer 2: o")
-win = False
-player = 1
-while not win:
-    col = int(input(f"\nPlayer {player}: Which column would you like to choose? "))
-    if player%2 == 1:
-        chip_type = "x"
-    else:
-        chip_type = "o"
-    row = insert_chip(board, col, chip_type)
+if __name__ == "__main__":
+    rows = int(input("What would you like the height of the board to be? "))
+    cols = int(input("What would you like the length of the board to be? "))
+    board = initialize_board(rows, cols)
     print_board(board)
-    if check_if_winner(board, col, row, chip_type):
-        win = True
-        print(f"Player {player} won the game!")
-        break
-    elif board[0].count("-") == 0:
-        win = True
-        print("Draw. Nobody wins.")
-        break
+    print("\n\nPlayer 1: x\nPlayer 2: o")
+    win = False
+    player = 1
+    while not win:
+        col = int(input(f"\nPlayer {player}: Which column would you like to choose? "))
+        if player%2 == 1:
+            chip_type = "x"
+        else:
+            chip_type = "o"
+        row = insert_chip(board, col, chip_type)
+        print_board(board)
+        if check_if_winner(board, col, row, chip_type):
+            win = True
+            print(f"Player {player} won the game!")
+            break
+        elif board[0].count("-") == 0:
+            win = True
+            print("Draw. Nobody wins.")
+            break
 
-    player += 1
-    if player % 2 == 1:
-        player = 1
-
+        player += 1
+        if player % 2 == 1:
+            player = 1
