ae4c23edfc24d98a8209c2ef46a68a598644f2e9
Austina247 <austinaanestad@gmail.com>
2025-10-13 22:34:04 -0400

    P2
    Signed-off-by: Austina247 <austinaanestad@gmail.com>

--- P2_A.py
+++ P2_A.py
@@ -5,7 +5,7 @@ def display_menu():
           "--------")
     print("0. Exit\n"
           "1. Load File\n"
-          "2. Lead Test Image\n"
+          "2. Load Test Image\n"
           "3. Read RLE String\n"
           "4. Read RLE Hex String\n"
           "5. Read Data Hex String\n"
@@ -15,8 +15,8 @@ def display_menu():
           "9. Display Hex Flat Data\n")
 
 def main():
-    print("Welcome to the RLE image encoder! \n")
-    print("Displaying Spectrum Image: ")
+    print("Welcome to the RLE image encoder!\n")
+    print("Displaying Spectrum Image:")
     console_gfx.display_image(console_gfx.test_rainbow)
     while True:
         display_menu()
