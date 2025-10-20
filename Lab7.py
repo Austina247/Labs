61bc9d6f09a4d664fce383e73915e8a8c7ddedad
Austina247 <austinaanestad@gmail.com>
2025-10-19 21:53:59 -0400

    P2
    Signed-off-by: Austina247 <austinaanestad@gmail.com>

--- Lab7.py
+++ Lab7.py
@@ -6,4 +6,10 @@ def reformat(data):
         dic1[dict["type"]][dict["name"]] = dict["price"]
     return dic1
 
+def nth(data, n):
+    if data is None:
+        return None
+    if n == 0:
+        return data[0]
+    return nth(data[1], n-1)
 
