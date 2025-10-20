c0c58e7f1822fea860c5fc20318a154c2eb8d8ff
Austina247 <austinaanestad@gmail.com>
2025-10-19 22:02:42 -0400

    P2
    Signed-off-by: Austina247 <austinaanestad@gmail.com>

--- Lab7.py
+++ Lab7.py
@@ -13,3 +13,19 @@ def nth(data, n):
         return data[0]
     return nth(data[1], n-1)
 
+def where(data):
+    if type(data) == str:
+        return 1 if data == "Waldo" else 0
+    elif type(data) == list:
+        total = 0
+        for name in data:
+            total += where(name)
+        return total
+    elif type(data) == dict:
+        total = 0
+        for key in data:
+            total += where(data[key])
+            total += where(key)
+        return total
+    else:
+        return 0
