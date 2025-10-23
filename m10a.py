096f0f36bf0a8a3f584896edd1ceafb8a345abeb
Austin <austinaanestad@gmail.com>
2025-10-23 11:12:38 -0400

    In class assignment

--- m10a.py
+++ m10a.py
@@ -26,13 +26,13 @@ class BankAccount:
     def display(self):
         print(f"Current balance: ${self.balance}")
 
-class Coordinates:
+class Coordinate:
     def __init__(self, x, y):
         self.x = x
         self.y = y
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
     def __add__(self, other):
-        return Coordinates(self.x + other.x, self.y + other.y)
+        return Coordinate(self.x + other.x, self.y + other.y)
     def __str__(self):
         return f"({self.x}, {self.y})"
